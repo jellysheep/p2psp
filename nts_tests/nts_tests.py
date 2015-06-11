0b8561bd198a3eefe20e8930b367e5443dce7b24
Max Mertens <max.mail@dameweb.de>
2015-06-11 17:19:32 +0200

    Added peer-to-peer communication.

--- nts_tests/nts_tests.py
+++ nts_tests/nts_tests.py
@@ -17,12 +17,15 @@ MSG_PACK = '4si'
 
 running = True
 
+
 def address_to_string(addr):
     return struct.pack(MSG_PACK, ipaddress.ip_address(unicode(addr[0])).packed, addr[1])
+
 
 def string_to_address(string):
     data = struct.unpack(MSG_PACK, string)
     return (str(ipaddress.ip_address(data[0]).exploded), data[1])
+
 
 def udp_splitter():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -55,7 +58,6 @@ def udp_splitter():
 
 def udp_peer():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    peers = []
 
     # connect to splitter
     time.sleep(0.5)  # wait for splitter to start
@@ -63,31 +65,34 @@ def udp_peer():
     data = sock.recv(1024)
     assert data == MSG_SPLITTER_HELLO
 
-    # get public address and port
+    # get public address and port of this peer
     data = sock.recv(1024)
     assert data.startswith(MSG_SPLITTER_SEND_ADDR)
+    public_address = string_to_address(data[len(MSG_SPLITTER_SEND_ADDR):])
+    print 'peer: got public address %s' % (public_address,)
 
-    # receive other peers' addresses
-    while running:
-        try:
-            while True:
-                data = sock.recv(1024, socket.MSG_DONTWAIT)
-                assert data.startswith(MSG_SPLITTER_NEW_PEER)
-                new_peer = string_to_address(data[len(MSG_SPLITTER_NEW_PEER):])
-                print 'peer: new peer %s' % (new_peer,)
-        except socket.error as e:
-            if e.errno != errno.EAGAIN:
-                raise
+    # receive other peer's address
+    data = sock.recv(1024)
+    assert data.startswith(MSG_SPLITTER_NEW_PEER)
+    peer = string_to_address(data[len(MSG_SPLITTER_NEW_PEER):])
+    print 'peer %s: new peer %s' % (public_address, peer)
+
+    time.sleep(0.5)
+    # connect to peer
+    sock.sendto(MSG_PEER_HELLO, peer)
+    data, reply_peer = sock.recvfrom(1024)
+    print 'peer %s: received "%s" from %s' % (public_address, data, reply_peer)
 
     sock.close()
 
 
-threads = [threading.Thread(target=udp_peer),
-           threading.Thread(target=udp_peer),
-           threading.Thread(target=udp_splitter)]
-for thread in threads:
+splitter_thread = threading.Thread(target=udp_splitter)
+splitter_thread.start()
+peer_threads = [threading.Thread(target=udp_peer), threading.Thread(target=udp_peer)]
+for thread in peer_threads:
     thread.start()
-time.sleep(5)
-running = False
-for thread in threads:
+
+for thread in peer_threads:
     thread.join()
+running = False
+splitter_thread.join()
