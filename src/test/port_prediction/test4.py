a60d3709af979a5f18f7ad8549a4afa1e88a82be
Max Mertens <max.mail@dameweb.de>
2016-06-16 16:33:52 +0200

    Added more lines to existing plots.

--- src/test/port_prediction/test4.py
+++ src/test/port_prediction/test4.py
@@ -16,5 +16,11 @@ for num_monitors in range(0, 11):
             incorporated_peers, max_predicted_ports, skip_likeliness_monitor, skip_likeliness_peer)
     success_percentage_port_step_2 = test_success_rate(number_of_runs, num_monitors, 2,
             incorporated_peers, max_predicted_ports, skip_likeliness_monitor, skip_likeliness_peer)
-    f.write("{0}\t{1}\t{2}\n".format(num_monitors, success_percentage_port_step_1, success_percentage_port_step_2))
+    if num_monitors < 5:
+        success_percentage_port_step_6 = test_success_rate(number_of_runs, num_monitors, 6,
+                incorporated_peers, max_predicted_ports, skip_likeliness_monitor, skip_likeliness_peer)
+        f.write("{0}\t{1}\t{2}\t{3}\n".format(num_monitors, success_percentage_port_step_1, success_percentage_port_step_2,
+                success_percentage_port_step_6))
+    else:
+        f.write("{0}\t{1}\t{2}\n".format(num_monitors, success_percentage_port_step_1, success_percentage_port_step_2))
 f.close()
--- src/test/port_prediction/test5.py
+++ src/test/port_prediction/test5.py
@@ -11,9 +11,11 @@ max_predicted_ports = 10
 
 f = open("results5.txt", 'w')
 for port_step in range(1, 11):
+    success_percentage_0_mon = test_success_rate(number_of_runs, 0, port_step,
+            incorporated_peers, max_predicted_ports, skip_likeliness_monitor, skip_likeliness_peer)
     success_percentage_1_mon = test_success_rate(number_of_runs, 1, port_step,
             incorporated_peers, max_predicted_ports, skip_likeliness_monitor, skip_likeliness_peer)
     success_percentage_2_mon = test_success_rate(number_of_runs, 2, port_step,
             incorporated_peers, max_predicted_ports, skip_likeliness_monitor, skip_likeliness_peer)
-    f.write("{0}\t{1}\t{2}\n".format(port_step, success_percentage_1_mon, success_percentage_2_mon))
+    f.write("{0}\t{1}\t{2}\t{3}\n".format(port_step, success_percentage_0_mon, success_percentage_1_mon, success_percentage_2_mon))
 f.close()
